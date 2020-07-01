250f5d46b8c0e58bea301ae55de4ee43d93f9772
Marcus Hellberg <marcus@vaadin.com>
2020-07-01 15:11:59 -0700

    Use prism for highlighting code

--- frontend/code-viewer.ts
+++ frontend/code-viewer.ts
@@ -5,17 +5,20 @@ import {
   html,
   LitElement,
   property,
-  query,
+  unsafeCSS,
 } from "lit-element";
+import { unsafeHTML } from "lit-html/directives/unsafe-html";
 import { getSimpleName } from "./util";
 import { getSource } from "./generated/RecipeEndpoint";
 import "@vaadin/vaadin-text-field/vaadin-text-area";
-import { TextAreaElement } from "@vaadin/vaadin-text-field/vaadin-text-area";
+//@ts-ignore
+import * as Prism from "./prism.js";
+import prismCss from "./prism.css";
 
 @customElement("code-viewer")
 export class CodeViewer extends LitElement {
   @property({ type: Array })
-  _files: string[] = [];
+  private _files: string[] = [];
 
   set files(files: string[]) {
     this._files = files;
@@ -25,36 +28,45 @@ export class CodeViewer extends LitElement {
     return this._files;
   }
   @property({ type: String })
-  contents: string = "";
-  viewSourceBound = this.viewSource.bind(this);
+  contents = "";
 
-  @query("vaadin-text-area")
-  codeViewer: TextAreaElement | undefined;
+  @property({ type: String })
+  language = "none";
 
   static get styles() {
-    return css`
-      :host {
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-      }
-      vaadin-tabs {
-        width: 100%;
-      }
-      vaadin-text-area {
-        width: 100%;
-        overflow: auto;
-      }
-    `;
+    return [
+      css`
+        ${unsafeCSS(prismCss)}
+      `,
+      css`
+        :host {
+          height: 100%;
+          display: flex;
+          flex-direction: column;
+        }
+        vaadin-tabs {
+          width: 100%;
+        }
+        pre {
+          width: 100%;
+          overflow: auto;
+          padding: var(--lumo-space-m);
+        }
+      `,
+    ];
   }
+
   render() {
     return html`
-      <vaadin-tabs @selected-changed=${this.viewSourceBound}
+      <vaadin-tabs @selected-changed=${this.viewSource}
         >${this.files.map(
           (file) => html`<vaadin-tab>${getSimpleName(file)}</vaadin-tab>`
         )}</vaadin-tabs
       >
-      <vaadin-text-area .value=${this.contents}></vaadin-text-area>
+      ${/*Don't reuse these elements. This is needed because Prism 
+          removes the markers lit-html uses to track slots */
+      unsafeHTML(`<pre><code class="language-${this.language}">
+          ${this.escapeHtml(this.contents)}</code></pre>`)}
     `;
   }
 
@@ -70,8 +82,24 @@ export class CodeViewer extends LitElement {
       }, 500);
       return;
     }
-    this.contents = await getSource(this.files[index]);
-    this.codeViewer!.scrollTop = 0;
+
+    const file = this.files[index];
+    this.contents = await getSource(file);
+    this.language = file.split(".").reverse()[0];
+
+    // Wait for LitElement to finish updating the DOM before higlighting
+    await this.updateComplete;
+    //@ts-ignore
+    Prism.highlightAllUnder(this.shadowRoot);
+  }
+
+  escapeHtml(unsafe: string) {
+    return unsafe
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;");
   }
 
   forceRefresh() {
