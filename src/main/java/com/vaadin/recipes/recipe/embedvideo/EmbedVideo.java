362a4c27430922b458f3387d362a6a887a709ebf
Jens Jansson <Peppe@users.noreply.github.com>
2020-08-18 15:52:45 +0300

    Embed video player (#28)

---
+++ src/main/java/com/vaadin/recipes/recipe/embedvideo/EmbedVideo.java
@@ -0,0 +1,17 @@
+package com.vaadin.recipes.recipe.embedvideo;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.router.Route;
+import com.vaadin.recipes.recipe.Metadata;
+import com.vaadin.recipes.recipe.Recipe;
+
+@Route("embed-video")
+@Metadata(howdoI = "Show a video")
+public class EmbedVideo extends Recipe {
+
+    public EmbedVideo() {
+        Video video = new Video("videos/vaadin-channel.mp4");
+        video.setMaxWidth("500px");
+        add(video);
+    }
+}
---
+++ src/main/java/com/vaadin/recipes/recipe/embedvideo/Video.java
@@ -0,0 +1,28 @@
+package com.vaadin.recipes.recipe.embedvideo;
+
+import com.vaadin.flow.component.*;
+
+@Tag("video")
+public class Video extends HtmlContainer {
+
+    private static final PropertyDescriptor<String, String> srcDescriptor = PropertyDescriptors
+            .attributeWithDefault("src", "");
+
+    public Video() {
+        super();
+        getElement().setProperty("controls", true);
+    }
+
+    public Video(String src) {
+        setSrc(src);
+        getElement().setProperty("controls", true);
+    }
+
+    public String getSrc() {
+        return get(srcDescriptor);
+    }
+
+    public void setSrc(String src) {
+        set(srcDescriptor, src);
+    }
+}
