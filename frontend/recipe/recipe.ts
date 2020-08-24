a92e612821df98bafb405d1d464f73ced5136785
Artur Signell <artur@vaadin.com>
2020-08-24 11:47:38 +0300

    Remove extra logging

--- frontend/recipe/recipe.ts
+++ frontend/recipe/recipe.ts
@@ -1,8 +1,6 @@
 import { LitElement } from "lit-element";
 import RecipeInfo from "../generated/com/vaadin/recipes/data/RecipeInfo";
 
-export const recipeInfo = (_info: RecipeInfo) => (_constructor: Function) => {
-  console.log(_constructor);
-};
+export const recipeInfo = (_info: RecipeInfo) => (_constructor: Function) => {};
 
 export class Recipe extends LitElement {}
