0b9ffb19c7a0de4fa8026600d5f4946fbd3eccf0
Matti Hosio <mhosio@vaadin.com>
2020-08-27 13:47:25 +0300

    Recipe for scrolling to a specific item in treegrid

---
+++ src/main/java/com/vaadin/recipes/recipe/scrolltoitemintreegrid/ScrollToItemInTreeGrid.java
@@ -0,0 +1,144 @@
+package com.vaadin.recipes.recipe.scrolltoitemintreegrid;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.treegrid.TreeGrid;
+import com.vaadin.flow.data.provider.hierarchy.HierarchicalDataCommunicator;
+import com.vaadin.flow.data.provider.hierarchy.HierarchicalDataProvider;
+import com.vaadin.flow.data.provider.hierarchy.HierarchyMapper;
+import com.vaadin.flow.function.SerializableConsumer;
+import com.vaadin.flow.router.Route;
+import com.vaadin.recipes.recipe.Metadata;
+import com.vaadin.recipes.recipe.Recipe;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+@Route("scroll-to-item-in-tree-grid")
+@Metadata(howdoI = "Scroll to a specific item when expanding nodes")
+public class ScrollToItemInTreeGrid extends Recipe {
+
+    public static class ScrollTreeGrid<T> extends TreeGrid<T> {
+
+        public ScrollTreeGrid(Class<T> beanType) {
+            super(beanType);
+            initScrollWhenReady();
+        }
+
+        public ScrollTreeGrid() {
+            super();
+            initScrollWhenReady();
+        }
+
+        public ScrollTreeGrid(HierarchicalDataProvider<T, ?> dataProvider) {
+            super(dataProvider);
+            initScrollWhenReady();
+        }
+
+        /** The method for scrolling to an item. Takes into account lazy loading nature of grid and does the scroll
+         * operation only until the grid has finished loading data
+         *
+         * @param item the item where to scroll to
+         */
+        public void scrollToItem(T item){
+            int index = getIndexForItem(item);
+            if(index>=0){
+                this.getElement().executeJs("this.scrollWhenReady($0);", index);
+            }
+        }
+
+        /** This is a method for getting the row index of an item in a treegrid. This works but is prone to break in the future versions due to
+         * its usage of reflection to access private methods to get access to the index.
+         *
+         * @param <T>
+         */
+        private int getIndexForItem(T item) {
+            HierarchicalDataCommunicator<T> dataCommunicator = super.getDataCommunicator();
+            Method getHierarchyMapper = null;
+            try {
+                getHierarchyMapper = HierarchicalDataCommunicator.class.getDeclaredMethod("getHierarchyMapper");
+                getHierarchyMapper.setAccessible(true);
+                HierarchyMapper<T, ?> mapper = (HierarchyMapper) getHierarchyMapper.invoke(dataCommunicator);
+                return mapper.getIndex(item);
+            } catch (Exception e) {
+                //TODO: handle
+                e.printStackTrace();
+            }
+            return -1;
+        }
+
+        private void initScrollWhenReady() {
+            runBeforeClientResponse(ui -> getElement().executeJs("this.scrollWhenReady = function(index){" +
+                    "if(this.loading) {var that = this; setTimeout(function(){that.scrollWhenReady(index);}, 200);}" +
+                    "        else {this.scrollToIndex(index);}" +
+                    "};"));
+        }
+
+
+        private void runBeforeClientResponse(SerializableConsumer<UI> command) {
+            getElement().getNode().runWhenAttached(ui -> ui
+                    .beforeClientResponse(this, context -> command.accept(ui)));
+        }
+    }
+
+    /** Data entiry to use in the treegrid
+     *
+     */
+    public static class Person {
+        private String name;
+        private Person parent;
+
+        public Person(){}
+
+        public Person(String name, Person parent) {
+            this.name = name;
+            this.parent = parent;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Person getParent() {
+            return parent;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setParent(Person parent) {
+            this.parent = parent;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+
+    }
+
+    /** Demo
+     *
+     */
+    public ScrollToItemInTreeGrid(){
+        ScrollTreeGrid<Person> grid = new ScrollTreeGrid<>();
+        grid.setWidth("90%");
+        grid.addHierarchyColumn(p -> p.getName()).setHeader("Name");
+        Person grandDad = new Person("Granddad", null);
+        Person dad = new Person("Dad", grandDad);
+        List<Person> people = new ArrayList<>();
+        people.add(grandDad);
+        people.add(dad);
+        for(int i=0;i<100;i++){
+            Person child = new Person("Child "+i, dad);
+            people.add(child);
+        }
+        Person lastChild = new Person("Last Child", dad);
+        people.add(lastChild);
+        people.forEach(p -> grid.getTreeData().addItem(p.getParent(), p));
+        grid.addExpandListener(e -> grid.scrollToItem(lastChild));
+        add(grid);
+    }
+}
