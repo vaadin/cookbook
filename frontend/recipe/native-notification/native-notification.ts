a33e8fd04dd007e757153eb5defcca7bdd929726
Viktor Lukashov <viktor@vaadin.com>
2020-08-30 01:30:22 +0300

    add a 'native-notification' recipe (#60)
    * add a 'native-notification' recipe
    
    The recipe shows how to display a native OS-level notification, without using a Service Worker.
    
    * address review comments
    
     - check that Notification exists on window
     - don't call requestPermission() if permission is already granted
    - bind field values to properties instead of reading them from the DOM directly
     - remove a wrong property type
    
    * make it work in Safari (which has a depricated API)
    
    More details: https://developer.mozilla.org/en-US/docs/Web/API/Notifications_API/Using_the_Notifications_API

---
+++ frontend/recipe/native-notification/native-notification.ts
@@ -0,0 +1,97 @@
+import { css, customElement, html, property } from "lit-element";
+import { nothing } from "lit-html";
+import { Recipe, recipeInfo } from "../recipe";
+import "@vaadin/vaadin-text-field/vaadin-text-field";
+import "@vaadin/vaadin-text-field/vaadin-text-area";
+import "@vaadin/vaadin-button/vaadin-button";
+
+@recipeInfo({
+  url: "native-notification",
+  howDoI: "Show a native OS-level notification",
+  description:
+    "Show how you display a native OS-level notification to the user.",
+})
+@customElement("native-notification")
+export class NativeNotification extends Recipe {
+
+  @property()
+  private error?: string;
+
+  private _title = "";
+  private _body = "";
+
+  render() {
+    return html`
+      <div class="flex-column">
+        <vaadin-text-field label="Title"
+          @value-changed=${this.onTitleChanged}></vaadin-text-field>
+        <vaadin-text-area label="Body"
+          @value-changed=${this.onBodyChanged}></vaadin-text-area>
+      </div>
+      <vaadin-button @click=${this.showNotification}>
+        Show Notification
+      </vaadin-button>
+
+      ${this.error ? html`
+        <p>The user has not allowed this page to show notifications:
+        ${this.error}</p>
+      ` : nothing}
+    `;
+  }
+
+  async showNotification() {
+    if (!('Notification' in window)) {
+      this.error = 'This browser does not support native notifications.'
+      return;
+    }
+
+    // Showing native notificaitons requires an explicit user permission
+    // https://developer.mozilla.org/en-US/docs/Web/API/Notification/requestPermission
+    let permission = Notification.permission;
+    if (permission !== "granted") {
+      // Safari uses the deprecated .requestPermission(callback) API
+      // Other browsers use the standard .requestPermission() => Promise API
+      let permissionCallback: NotificationPermissionCallback;
+      const permissionSafari = new Promise<NotificationPermission>(r => permissionCallback = r);
+      const permissionModern = Notification.requestPermission(permissionCallback!);
+
+      // If `permissionModern` is defined use it (Safari would return `undefined`).
+      // Otherwise fallback to the callback-based API.
+      permission = await (permissionModern || permissionSafari);
+    }
+
+    switch (permission) {
+      case "granted":
+        // For how to add icon, badge, image, actions and more
+        // see https://developer.mozilla.org/en-US/docs/Web/API/Notification/Notification
+        const notification = new Notification(this._title, { body: this._body });
+        notification.onclick = e => console.log("clicked", e);
+        this.error = undefined;
+        break;
+      case "denied":
+      case "default":
+          this.error = permission;
+    }
+  }
+
+  private onTitleChanged(e: CustomEvent) {
+    this._title = (e.target as HTMLInputElement).value;
+  }
+
+  private onBodyChanged(e: CustomEvent) {
+    this._body = (e.target as HTMLInputElement).value;
+  }
+
+  static styles = css`
+    :host {
+      display: block;
+      margin: 20px;
+    }
+
+    .flex-column {
+      display: flex;
+      flex-direction: column;
+      max-width: 600px;
+    }
+  `;
+}
