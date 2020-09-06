847366236497fdedddff1efea5c6a28095d1c452
Jouni Koivuviita <jouni@vaadin.com>
2020-09-06 19:18:09 +0300

    Design update (#75)

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,6 +1,8 @@
 import { Flow } from "@vaadin/flow-frontend/Flow";
 import { Route, Router } from "@vaadin/router";
 import { tsRecipeRoutes } from "./ts-routes";
+import RecipeInfo from "./generated/com/vaadin/recipes/data/RecipeInfo";
+import * as RecipeEndpoint from "./generated/RecipeEndpoint";
 
 const { serverSideRoutes } = new Flow({
   imports: () => import("../target/frontend/generated-flow-imports"),
@@ -8,25 +10,38 @@ const { serverSideRoutes } = new Flow({
 
 const routes: Route[] = [
   {
-    path: "",
-    component: "main-view",
+    path: "/",
+    component: "recipes-list-view",
     action: async (_context, _commands) => {
-      await import("./views/main-view");
+      await initRecipes();
+      await import("./views/recipes-list-view");
+    },
+  },
+  {
+    path: "",
+    component: "recipe-view",
+    action: async (_context, _commands) => {
+      await initRecipes();
+      await import("./views/recipe-view");
     },
     children: [
-      {
-        path: "",
-        component: "intro-view",
-        action: async () => {
-          await import("./views/intro-view");
-        },
-      },
       ...tsRecipeRoutes,
       ...serverSideRoutes, // IMPORTANT: this must be the last entry in the array
     ],
-  },
+  }
 ];
 
 export const router = new Router(document.querySelector("#outlet"));
 router.setRoutes(routes);
 
+export const recipes: RecipeInfo[] = [];
+
+async function initRecipes() {
+  if (recipes.length === 0) {
+    recipes.push(...tsRecipeRoutes.map((route) => route.info));
+    recipes.push(...(await RecipeEndpoint.list()));
+    recipes.sort((a, b) =>
+      a.howDoI < b.howDoI ? -1 : a.howDoI == b.howDoI ? 0 : 1
+    );
+  }
+}
---
+++ frontend/views/recipe-view.ts
@@ -0,0 +1,114 @@
+import { css, customElement, html, LitElement, property } from "lit-element";
+import { render, TemplateResult } from "lit-html";
+import "../code-viewer";
+import RecipeInfo from "../generated/com/vaadin/recipes/data/RecipeInfo";
+import { recipes } from "../";
+
+@customElement("recipe-view")
+export class RecipeView extends LitElement {
+  @property({ type: Object })
+  recipe: RecipeInfo = { howDoI: "", url: "", tags: [] };
+
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+        padding-bottom: var(--space-lg);
+      }
+
+      ::slotted([slot="header"]) {
+        background-color: var(--color-white);
+        padding-top: var(--space-sm);
+        padding-bottom: var(--space-sm);
+      }
+
+      ::slotted(:not([slot]).container-fluid) {
+        display: block;
+        min-height: 100px;
+        padding-top: var(--space-md);
+        padding-bottom: var(--space-md);
+      }
+
+      ::slotted(.flow-example) {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+    `;
+  }
+
+  async onAfterEnter(context:any) {
+    const tag = context.pathname.substr(context.pathname.lastIndexOf("/") + 1);
+    const recipe = recipes.find((recipe) => recipe.url == tag);
+    if (recipe) {
+      this.recipe = recipe;
+      this.renderLightDom();
+      const example = this.querySelector(":not([slot])");
+      example?.classList.add("container-fluid");
+      if (example?.nodeName.startsWith("flow-container-root")) {
+        example.classList.add("flow-example");
+      }
+    }
+  }
+
+  render() {
+    return html`
+      <slot name="header"></slot>
+      <div class="example">
+        <slot></slot>
+      </div>
+      <slot name="code"></slot>
+    `;
+  }
+
+  renderLightDom() {
+    let headerSlot = this.querySelector("[slot=header]");
+    if (!headerSlot) {
+      headerSlot = document.createElement("div");
+      headerSlot.setAttribute("slot", "header");
+      headerSlot.classList.add("recipe-view-header");
+      this.appendChild(headerSlot);
+    }
+    render(headerTemplate(this.recipe), headerSlot);
+
+    let codeSlot = this.querySelector("[slot=code]");
+    if (!codeSlot) {
+      codeSlot = document.createElement("div");
+      codeSlot.setAttribute("slot", "code");
+      codeSlot.classList.add("recipe-view-code", "container-fluid");
+      this.appendChild(codeSlot);
+    }
+    render(codeTemplate(this.recipe), codeSlot);
+  }
+}
+
+const headerTemplate: (data: any) => TemplateResult = (recipe: RecipeInfo) => html`
+  <style>
+    .recipe-view-back-link {
+      font-weight: 600;
+      text-decoration: none !important;
+      margin-left: -1.25em;
+    }
+    .recipe-view-title {
+      margin-bottom: var(--space-xs);
+      padding-top: var(--space-sm);
+    }
+  </style>
+  <div class="container-fluid">
+    <a href="/" class="recipe-view-back-link">
+      <div class="icon-wrapper">
+        <i class="las la-arrow-left" aria-hidden="true"></i>
+      </div>
+      <span>Back to Cookbook</span>
+    </a>
+    <h3 class="recipe-view-title">${recipe.howDoI.trim().replace(/^\w/, (c) => c.toUpperCase())}</h3>
+    <p class="paragraph-sm" ?hidden=${recipe.description?.length===0}>${recipe.description}</p>
+    ${recipe.tags?.map(
+      (tag) => html`<span class="tag">${tag}</span> `
+    )}
+  </div>
+`;
+
+const codeTemplate: (data: any) => TemplateResult = (recipe: RecipeInfo) => html`
+  <code-viewer .files=${recipe.sourceFiles || []}></code-viewer>
+`;
