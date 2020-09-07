1de32294cfd474d614880bab3926828615dcc483
Artur Signell <artur@vaadin.com>
2020-09-07 09:45:02 +0300

    Fix title setting for TabsWithRoutes
    Make sure all Java recipes extend Recipe

--- src/main/java/com/vaadin/recipes/AppShell.java
+++ src/main/java/com/vaadin/recipes/AppShell.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.router.NavigationState;
+import com.vaadin.flow.router.RouterLayout;
 import com.vaadin.flow.server.AppShellSettings;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.server.VaadinRequest;
@@ -11,9 +12,12 @@ import com.vaadin.recipes.data.AllRecipes;
 import com.vaadin.recipes.data.RecipeInfo;
 import com.vaadin.recipes.recipe.Recipe;
 
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -43,27 +47,63 @@ public class AppShell implements AppShellConfigurator {
     @Override
     public void configurePage(AppShellSettings settings) {
         VaadinRequest request = settings.getRequest();
-        String pathInfo = request.getPathInfo();
-        Optional<NavigationState> target = request.getService().getRouter().resolveNavigationTarget(pathInfo,
-                request.getParameterMap());
+        RecipeInfo recipeInfo = findServerSideRecipe(request);
+        if (recipeInfo == null) {
+            recipeInfo = findTypeScriptRecipe(request);
+        }
 
-        if (target.isPresent() && Recipe.class.isAssignableFrom(target.get().getNavigationTarget())) {
-            // Server side route
-            Class<? extends Recipe> recipeClass = (Class<? extends Recipe>) target.get().getNavigationTarget();
-            RecipeInfo recipeInfo = AllRecipes.getRecipeInfo(recipeClass);
+        if (recipeInfo != null) {
             String howDoI = getHowDoI(recipeInfo);
             setRouteMeta(settings, howDoI, recipeInfo.getDescription());
         } else {
-            RecipeInfo recipeInfo = tsRecipes.get(pathInfo);
-            if (recipeInfo == null && pathInfo != null && pathInfo.startsWith("/")) {
-                recipeInfo = tsRecipes.get(pathInfo.substring(1));
-            }
+            LoggerFactory.getLogger(getClass()).warn("No recipe info found for {}", request.getPathInfo());
+        }
+    }
 
-            if (recipeInfo != null) {
-                // TS route
-                setRouteMeta(settings, getHowDoI(recipeInfo), recipeInfo.getDescription());
+    private RecipeInfo findTypeScriptRecipe(VaadinRequest request) {
+        String pathInfo = request.getPathInfo();
+
+        RecipeInfo recipeInfo = tsRecipes.get(pathInfo);
+        if (recipeInfo == null && pathInfo != null && pathInfo.startsWith("/")) {
+            recipeInfo = tsRecipes.get(pathInfo.substring(1));
+        }
+
+        return recipeInfo;
+    }
+
+    private RecipeInfo findServerSideRecipe(VaadinRequest request) {
+        Optional<NavigationState> target = request.getService().getRouter()
+                .resolveNavigationTarget(request.getPathInfo(), request.getParameterMap());
+
+        if (!target.isPresent()) {
+            return null;
+        }
+        NavigationState navigationState = target.get();
+        Class<?> recipeClass = navigationState.getNavigationTarget();
+        if (!(Recipe.class.isAssignableFrom(recipeClass))) {
+            recipeClass = navigationState.getRouteTarget().getTarget();
+        }
+
+        return findRecipeClass(navigationState, recipeClass);
+    }
+
+    private RecipeInfo findRecipeClass(NavigationState navigationState, Class<?> recipeClass) {
+        if (recipeClass == null) {
+            return null;
+        }
+        if (Recipe.class.isAssignableFrom(recipeClass)) {
+            // Server side route
+            return AllRecipes.getRecipeInfo((Class<? extends Recipe>) recipeClass);
+        }
+
+        List<Class<? extends RouterLayout>> parents = navigationState.getRouteTarget().getParentLayouts();
+        for (Class<? extends RouterLayout> parent : parents) {
+            if (Recipe.class.isAssignableFrom(parent)) {
+                return AllRecipes.getRecipeInfo((Class<? extends Recipe>) parent);
             }
         }
+
+        return null;
     }
 
     private void setRouteMeta(AppShellSettings settings, String howDoI, String description) {
