4c9414c575f49d0fa471227d784409b6f6ee0781
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2020-09-10 10:07:58 +0300

    Add input event listener

--- src/main/java/com/vaadin/recipes/recipe/alwayseditablelabel/AlwaysEditableLabel.java
+++ src/main/java/com/vaadin/recipes/recipe/alwayseditablelabel/AlwaysEditableLabel.java
@@ -1,6 +1,7 @@
 package com.vaadin.recipes.recipe.alwayseditablelabel;
 
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.router.Route;
 import com.vaadin.recipes.recipe.Metadata;
 import com.vaadin.recipes.recipe.Recipe;
@@ -16,6 +17,11 @@ public class AlwaysEditableLabel extends Recipe {
     public AlwaysEditableLabel() {
         Label label = new Label("I'm an editable label ... ");
         label.getElement().setAttribute("contenteditable", true);
+
+        label.getElement().addEventListener("input",
+                e -> Notification.show("Value changed: "
+                        + e.getEventData().getString("event.target.innerHTML")))
+                .addEventData("event.target.innerHTML");
 
         add(label);
     }
