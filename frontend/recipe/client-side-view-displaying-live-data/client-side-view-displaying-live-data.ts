1cce1ad702e0b087d4b09bc87646859e1bd36757
Marcus Hellberg <marcus@vaadin.com>
2020-09-10 12:25:22 -0700

    client-side live data example

---
+++ frontend/recipe/client-side-view-displaying-live-data/client-side-view-displaying-live-data.ts
@@ -0,0 +1,55 @@
+import Tag from "../../generated/com/vaadin/recipes/recipe/Tag";
+import { recipeInfo, Recipe } from "../recipe";
+import { customElement, html, internalProperty } from "lit-element";
+
+import "@vaadin/vaadin-charts";
+
+@recipeInfo({
+  url: "client-side-view-displaying-live-data",
+  howDoI: "Show real-time updating content in a client-side view",
+  description:
+    "Learn how to automatically update the UI with data from a reactive backend.",
+  sourceFiles: [
+    "com/vaadin/recipes/recipe/clientsidelivedata/StockDataEndpoint.java",
+  ],
+  tags: [Tag.PUSH],
+})
+@customElement("client-side-view-displaying-live-data")
+export class ShortcutListener extends Recipe {
+  @internalProperty()
+  private ticker = "FOO";
+  @internalProperty()
+  private currentPrice = "";
+  @internalProperty()
+  private priceHistory: number[] = [];
+
+  render() {
+    return html`
+      <h2>${this.ticker} - $${this.currentPrice}</h2>
+      <vaadin-chart>
+        <vaadin-chart-series
+          title=${this.ticker}
+          .values=${this.priceHistory}
+        ></vaadin-chart-series>
+      </vaadin-chart>
+    `;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    // Connect to the backend
+    const priceSource = new EventSource(
+      `${window.location.origin}/stock-price`
+    );
+
+    // Listen for incoming data and update state
+    priceSource.addEventListener("message", (e) => {
+      this.currentPrice = JSON.parse(e.data);
+      this.priceHistory = [...this.priceHistory, parseFloat(this.currentPrice)];
+    });
+
+    // The backend only sends 30 points, close after that
+    priceSource.addEventListener("error", () => priceSource.close());
+  }
+}
---
+++ src/main/java/com/vaadin/recipes/recipe/clientsidelivedata/StockDataEndpoint.java
@@ -0,0 +1,26 @@
+package com.vaadin.recipes.recipe.clientsidelivedata;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.util.Random;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+public class StockDataEndpoint {
+
+    @GetMapping(path = "/stock-price", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> streamFlux() {
+        Random random = new Random();
+        return Flux
+            .<String>generate(
+                sink -> {
+                    sink.next(BigDecimal.valueOf(random.nextInt(10000), 2).toString());
+                }
+            )
+            .delayElements(Duration.ofMillis(500))
+            .take(30);
+    }
+}
