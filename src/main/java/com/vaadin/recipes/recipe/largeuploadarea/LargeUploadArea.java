eb1eb9838f43a07c93d8c267cb4593ea96701a50
Olli Tietäväinen <ollit@vaadin.com>
2020-09-22 12:38:59 +0300

    changed TextArea to a VerticalLayout where you can drop files and tokens appear

--- src/main/java/com/vaadin/recipes/recipe/largeuploadarea/LargeUploadArea.java
+++ src/main/java/com/vaadin/recipes/recipe/largeuploadarea/LargeUploadArea.java
@@ -1,6 +1,12 @@
 package com.vaadin.recipes.recipe.largeuploadarea;
 
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.upload.Upload;
 import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
@@ -12,6 +18,8 @@ import java.io.InputStream;
 
 @Route("large-upload-area")
 @CssImport(value = "./recipe/large-upload-area/large-upload-area.css", themeFor = "vaadin-upload")
+@JsModule("@vaadin/vaadin-lumo-styles/badge.js")
+@CssImport(value = "./recipe/large-upload-area/file.css", include = "lumo-badge")
 @Metadata(
     howdoI = "Create a large upload drop area",
     sourceFiles = { "recipe/large-upload-area/large-upload-area.css" },
@@ -25,32 +33,38 @@ public class LargeUploadArea extends Recipe {
         upload.getElement().getThemeList().add("custom-upload");
         upload.setWidth("100%");
         upload.setHeight("500px");
-        upload.setAcceptedFileTypes("text/*");
         upload.setMaxFileSize(100000);
         upload.setMaxFiles(20);
 
-        TextArea textArea = new TextArea();
-        textArea.setValue("You can type or drop small text files here!");
-        textArea.setSizeFull();
+        VerticalLayout verticalLayout = new VerticalLayout();
+        verticalLayout.setClassName("drop-area");
+        HorizontalLayout fileContainerLayout = new HorizontalLayout();
+        verticalLayout.add(new Span("You drag and drop small files here!"), fileContainerLayout);
+
+        verticalLayout.setSizeFull();
 
         upload.addSucceededListener(
             event -> {
-                String value = textArea.getValue();
-                if (value == null || value.isEmpty()) {
-                    value = "";
-                } else {
-                    value += "\n";
-                }
-                InputStream inputStream = buffer.getInputStream();
-                try {
-                    value += new String(inputStream.readAllBytes());
-                } catch (IOException e) {
-                    value += e.getMessage();
-                }
-                textArea.setValue(value);
+                Div droppedFile = new Div();
+                droppedFile.setClassName("file-div");
+                Span deleteBadge = new Span("X");
+                deleteBadge.setClassName("delete-button");
+                deleteBadge.getElement().getThemeList().add("badge contrast pill");
+                deleteBadge
+                    .getElement()
+                    .addEventListener(
+                        "click",
+                        e -> {
+                            fileContainerLayout.remove(droppedFile);
+                        }
+                    );
+                Span fileNameSpan = new Span();
+                fileNameSpan.setText(event.getFileName());
+                droppedFile.add(deleteBadge, fileNameSpan);
+                fileContainerLayout.add(droppedFile);
             }
         );
-        upload.getElement().appendChild(textArea.getElement());
+        upload.getElement().appendChild(verticalLayout.getElement());
 
         add(upload);
     }
