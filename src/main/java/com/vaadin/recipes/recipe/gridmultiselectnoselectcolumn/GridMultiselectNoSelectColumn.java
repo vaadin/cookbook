b5b27921346f8a014137998eb85c65dea27e1f08
Marcus Hellberg <marcus@vaadin.com>
2020-12-03 12:58:52 -0800

    move pojo down to make code easier to scan

--- src/main/java/com/vaadin/recipes/recipe/gridmultiselectnoselectcolumn/GridMultiselectNoSelectColumn.java
+++ src/main/java/com/vaadin/recipes/recipe/gridmultiselectnoselectcolumn/GridMultiselectNoSelectColumn.java
@@ -1,11 +1,5 @@
 package com.vaadin.recipes.recipe.gridmultiselectnoselectcolumn;
 
-import java.time.LocalDate;
-import java.util.Collection;
-
-import org.vaadin.artur.exampledata.DataType;
-import org.vaadin.artur.exampledata.ExampleDataGenerator;
-
 import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.Grid.SelectionMode;
@@ -13,6 +7,10 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.recipes.recipe.Metadata;
 import com.vaadin.recipes.recipe.Recipe;
 import com.vaadin.recipes.recipe.Tag;
+import java.time.LocalDate;
+import java.util.Collection;
+import org.vaadin.artur.exampledata.DataType;
+import org.vaadin.artur.exampledata.ExampleDataGenerator;
 
 @Route("grid-multiselect-no-selectcolumn")
 @Metadata(
@@ -21,8 +19,41 @@ import com.vaadin.recipes.recipe.Tag;
     tags = { Tag.GRID }
 )
 public class GridMultiselectNoSelectColumn extends Recipe {
-
     private Grid<Person> grid = new Grid<>(Person.class);
+
+    private Collection<Person> createExamplePersons(int count) {
+        ExampleDataGenerator<Person> generator = new ExampleDataGenerator<>(Person.class, 123);
+        generator.setData(Person::setFirstName, DataType.FIRST_NAME);
+        generator.setData(Person::setLastName, DataType.LAST_NAME);
+        generator.setData(Person::setBirthDate, DataType.DATE_OF_BIRTH);
+        return generator.create(count);
+    }
+
+    public GridMultiselectNoSelectColumn() {
+        // Setup a grid with random data
+        grid.setItems(createExamplePersons(100));
+        grid.setSelectionMode(SelectionMode.MULTI);
+
+        grid.addItemClickListener(
+            event -> {
+                Person item = event.getItem();
+                if (grid.getSelectedItems().contains(item)) {
+                    grid.deselect(item);
+                } else {
+                    grid.select(event.getItem());
+                }
+            }
+        );
+
+        add(grid);
+    }
+
+    public void onAttach(AttachEvent event) {
+        // Hide the selection column with JS
+        grid
+            .getElement()
+            .executeJs("this.getElementsByTagName(\"vaadin-grid-flow-selection-column\")[0].hidden = true;");
+    }
 
     public static class Person {
         private String firstName, lastName;
@@ -51,36 +82,5 @@ public class GridMultiselectNoSelectColumn extends Recipe {
         public void setLastName(String lastName) {
             this.lastName = lastName;
         }
-    }
-
-    private Collection<Person> createExamplePersons(int count) {
-        ExampleDataGenerator<Person> generator = new ExampleDataGenerator<>(
-                Person.class, 123);
-        generator.setData(Person::setFirstName, DataType.FIRST_NAME);
-        generator.setData(Person::setLastName, DataType.LAST_NAME);
-        generator.setData(Person::setBirthDate, DataType.DATE_OF_BIRTH);
-        return generator.create(count);
-    }
-
-    public GridMultiselectNoSelectColumn() {
-        // Setup a grid with random data
-        grid.setItems(createExamplePersons(100));
-        grid.setSelectionMode(SelectionMode.MULTI);
-
-        grid.addItemClickListener(event -> {
-            Person item = event.getItem();
-            if (grid.getSelectedItems().contains(item)) {
-                grid.deselect(item);
-            } else {
-                grid.select(event.getItem());
-            }
-        });
-
-        add(grid);
-    }
-
-    public void onAttach(AttachEvent event) {
-        grid.getElement().executeJs(
-                "this.getElementsByTagName(\"vaadin-grid-flow-selection-column\")[0].hidden = true;");
     }
 }
