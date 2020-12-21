a3fc270e22f49b91853dcb48a8cae270bde17d07
Jean-Christophe Gueriaud <jean-christophe@vaadin.com>
2020-12-21 17:02:16 +0200

    Add a recipe dense grid them

---
+++ src/main/java/com/vaadin/recipes/recipe/densegrid/GridDenseTheme.java
@@ -0,0 +1,69 @@
+package com.vaadin.recipes.recipe.densegrid;
+
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.router.Route;
+import com.vaadin.recipes.recipe.Metadata;
+import com.vaadin.recipes.recipe.Recipe;
+import com.vaadin.recipes.recipe.Tag;
+import org.vaadin.artur.exampledata.DataType;
+import org.vaadin.artur.exampledata.ExampleDataGenerator;
+
+import java.time.LocalDate;
+import java.util.Collection;
+
+@Route("grid-dense-theme")
+@Metadata(
+    howdoI = "Create a dense grid",
+    description = "Create a dense grid with compact rows",
+    tags = { Tag.GRID, Tag.THEME }
+)
+@CssImport(themeFor = "vaadin-grid", value = "recipe/densegrid/densegrid.css")
+public class GridDenseTheme extends Recipe {
+    private Grid<Person> grid = new Grid<>(Person.class);
+
+    private Collection<Person> createExamplePersons(int count) {
+        ExampleDataGenerator<Person> generator = new ExampleDataGenerator<>(Person.class, 123);
+        generator.setData(Person::setFirstName, DataType.FIRST_NAME);
+        generator.setData(Person::setLastName, DataType.LAST_NAME);
+        generator.setData(Person::setBirthDate, DataType.DATE_OF_BIRTH);
+        return generator.create(count);
+    }
+
+    public GridDenseTheme() {
+        // Setup a grid with random data
+        grid.setItems(createExamplePersons(100));
+        grid.setThemeName("dense");
+
+        add(grid);
+    }
+
+    public static class Person {
+        private String firstName, lastName;
+        private LocalDate birthDate;
+
+        public LocalDate getBirthDate() {
+            return birthDate;
+        }
+
+        public void setBirthDate(LocalDate birthDate) {
+            this.birthDate = birthDate;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+    }
+}
