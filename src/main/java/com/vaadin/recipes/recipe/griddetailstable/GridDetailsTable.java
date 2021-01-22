d2514fb4c8e0e31bd9f377fbf4c343abaa9a3a2f
tatulund <tatu@vaadin.com>
2021-01-22 17:21:24 +0200

    Generate html table in Grid details using TemplateRenderer

---
+++ src/main/java/com/vaadin/recipes/recipe/griddetailstable/GridDetailsTable.java
@@ -0,0 +1,62 @@
+package com.vaadin.recipes.recipe.griddetailstable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.data.renderer.TemplateRenderer;
+import com.vaadin.flow.router.Route;
+import com.vaadin.recipes.recipe.Metadata;
+import com.vaadin.recipes.recipe.Recipe;
+import com.vaadin.recipes.recipe.Tag;
+import com.vaadin.recipes.recipe.griddetailstable.MonthlyExpense.DailyExpenses;
+
+@Route("grid-details-table")
+@Metadata(howdoI = "Generate a html table in Grid details", description = "Grid itself is a heavy component and might be overkill to use in Grid details. Here we generate a light weight html table with TemplateRenderer to show details data of another bean type. This will keep the complexity of the view in control.", sourceFiles = {
+        "MonthlyExpense.java" }, tags = { Tag.GRID })
+public class GridDetailsTable extends Recipe {
+    private Grid<MonthlyExpense> grid = new Grid<>(MonthlyExpense.class);
+    private static String TD = "<td style=\"border: 1px solid lightgrey; width: 33.3%; padding: 3px;\">";
+    private static String TABLEHEADER = "<tr>" + TD + "<b>Day</b></td>" + TD
+            + "<b>Explanation</b></td>" + TD + "<b>Amount</b></td></tr>";
+
+    public GridDetailsTable() {
+        setHeight("500px");
+        grid.setItems(getData());
+        grid.setColumns("year", "month", "expenses");
+        // Create a TemplateRenderer, which is Div where we will add the Table
+        grid.setItemDetailsRenderer(TemplateRenderer.<MonthlyExpense> of(
+                "<div style=\"border: 1px solid gray; width: 100%;\"inner-h-t-m-l=\"[[item.html]]\"></div>")
+                .withProperty("html",
+                        // Generate Table for the DailyExpenses of the selected
+                        // month
+                        expense -> getTable(expense.getDailyExpenses())));
+        grid.setSizeFull();
+        add(grid);
+    }
+
+    private static List<MonthlyExpense> getData() {
+        String[] monthNames = new java.text.DateFormatSymbols().getMonths();
+        List<MonthlyExpense> data = new ArrayList<>();
+        for (int year = 2000; year < 2020; year++) {
+            for (int month = 0; month < 12; month++) {
+                data.add(new MonthlyExpense(monthNames[month], year));
+            }
+        }
+        return data;
+    }
+
+    private static String getTable(List<DailyExpenses> dailyExpenses) {
+        String table = "<table style=\"width: 100%\">" + TABLEHEADER
+        // Collect stream of DailyExpenses to a String for each row of the Table
+                + dailyExpenses.stream()
+                        .map(dailyExpense -> "<tr>" + TD + dailyExpense.getDay()
+                                + "</td>" + TD + dailyExpense.getExplanation()
+                                + "</td>" + TD + dailyExpense.getAmount()
+                                + "</td>")
+                        .collect(Collectors.joining("</tr>"))
+                + "</table>";
+        return table;
+    }
+}
---
+++ src/main/java/com/vaadin/recipes/recipe/griddetailstable/MonthlyExpense.java
@@ -0,0 +1,84 @@
+package com.vaadin.recipes.recipe.griddetailstable;
+
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+import java.util.Random;
+
+public class MonthlyExpense {
+    private String month;
+    private Double expenses;
+    private int year;
+    private Random random = new Random();
+    private List<DailyExpenses> dailyExpenses;
+
+    public MonthlyExpense(String month, int year) {
+        setMonth(month);
+        setExpenses(expenses);
+        setYear(year);
+        List<DailyExpenses> list = new ArrayList<>();
+        for (int i = 1; i < 31; i++) {
+            list.add(new DailyExpenses(i));
+        }
+        setDailyExpenses(list);
+        Double sum = list.stream().map(entry -> entry.getAmount()).reduce(0d,
+                Double::sum);
+        setExpenses(sum);
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public Double getExpenses() {
+        return expenses;
+    }
+
+    public void setExpenses(Double expenses) {
+        this.expenses = expenses;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    private void setDailyExpenses(List<DailyExpenses> list) {
+        dailyExpenses = list;
+
+    }
+
+    public List<DailyExpenses> getDailyExpenses() {
+        return dailyExpenses;
+    }
+
+    public class DailyExpenses {
+        private String explanation = "Explanation of the expense";
+        private Double amount = 0d;
+        private Integer day;
+
+        public DailyExpenses(Integer day) {
+            this.day = day;
+            this.amount = Math.floor((Math.random() * 1000) % 500 + 300);
+        }
+
+        public String getExplanation() {
+            return explanation;
+        }
+
+        public Double getAmount() {
+            return amount;
+        }
+
+        public Integer getDay() {
+            return day;
+        }
+    }
+}
