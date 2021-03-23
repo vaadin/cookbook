06ed68480aa6ad9f202a3c2c79c9512409330114
Marcus Hellberg <marcus@vaadin.com>
2021-03-23 12:13:02 -0700

    update to Vaadin 19

--- frontend/views/recipes-list-view.ts
+++ frontend/views/recipes-list-view.ts
@@ -94,7 +94,7 @@ export class RecipesListView extends LitElement {
 
   tags: Tag[] = [
     Tag.JAVA,
-    Tag.TYPESCRIPT,
+    Tag.TYPE_SCRIPT,
     Tag.FLOW,
     Tag.FUSION,
     Tag.KEYBOARD,
@@ -107,7 +107,7 @@ export class RecipesListView extends LitElement {
     Tag.LAYOUT,
     Tag.DOWNLOAD,
     Tag.FORM,
-    Tag.ACCESSIBILITY
+    Tag.ACCESSIBILITY,
   ];
 
   firstUpdated() {
