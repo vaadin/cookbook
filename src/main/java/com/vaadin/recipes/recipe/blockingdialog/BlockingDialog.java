589184b3ee79854961a08e7046e98a94a2b03bca
Enver Haase <enver@vaadin.com>
2021-05-31 13:06:38 +0200

    improved the thread locking, but can probably still be simplified

--- src/main/java/com/vaadin/recipes/recipe/blockingdialog/BlockingDialog.java
+++ src/main/java/com/vaadin/recipes/recipe/blockingdialog/BlockingDialog.java
@@ -6,7 +6,6 @@ import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.Command;
 import com.vaadin.recipes.recipe.Metadata;
 import com.vaadin.recipes.recipe.Recipe;
 import com.vaadin.recipes.recipe.Tag;
@@ -27,9 +26,6 @@ public class BlockingDialog extends Recipe {
     // This class can be copied into a utility class and is an inner class only because that way the code is
     // shown in the Vaadin Cookbook environment.
     public static class YesNoDecider {
-        abstract static class MyCommand implements Command {
-            volatile boolean done;
-        }
 
         // to be called from within a UI's thread
         public static void yesNo(String question, Consumer<Boolean> consumer) {
@@ -38,51 +34,40 @@ public class BlockingDialog extends Recipe {
 
             CompletableFuture<Boolean> future = new CompletableFuture<>();
             dialog.add(new Span(question));
-            dialog.add(new Button("yes", e -> future.complete(true)));
-            dialog.add(new Button("no", e -> future.complete(false)));
+            dialog.add(new Button("yes", e -> future.complete(true) ));
+            dialog.add(new Button("no", e -> future.complete(false) ));
             dialog.setModal(true); // in fact, the extra indirection through a command that is re-executed a number of times would allow for non-modality, too.
             dialog.setCloseOnEsc(false); // allowing to remove the yes/np buttons would keep the user-code un-executed forever, leaking memory and possibly other resources.
             dialog.setCloseOnOutsideClick(false); // allowing to remove the yes/no buttons would keep the user-code un-executed forever, leaking memory and possibly other resources.
-            dialog.open();
-
-            // Command will be run in the UI thread eventually
-            final YesNoDecider.MyCommand command = new YesNoDecider.MyCommand() {
-                @Override
-                public void execute() {
-                    if (future.isDone()) {
-                        try {
-                            consumer.accept(future.get());
-                            done = true;
-                        } catch (InterruptedException | ExecutionException ex) {
-                            ex.printStackTrace();
-                        }
-                    }
-                }
-            };
-
-            UI ui = UI.getCurrent(); // in the UI thread, get the UI instance
-            new Thread(() -> { // worker thread trying to let the UI thread run the command which in turn runs the user code as soon as the required user-input boolean is there
-                UI.setCurrent(ui); // in case the command uses 'UI.getCurrent()' assign the parent thread's UI to this new child thread.
-                while (!command.done) { // repeat until it is finally done.
-                    try {
-                        ui.access(command).get(); // and use the UI instance in a child thread, block this thread until the UI thread tried another time.
-                        // network usage: make sure that there is no polling set so that we only piggy-back upon the client-server communications that take
-                        // place after some user action such as a click.
-                        // Using Vaadin Push, we would not need such a piggyback: one could push UI changes out to the client using UI.push().
-                    } catch (InterruptedException | ExecutionException ex) {
-                        ex.printStackTrace();
-                    }
-                }
-            }).start(); // one may consider instead using a thread pool, see ExecutorService for example.
 
             // functional identity mapping with a little side effect
             future.thenApply(value -> {
                 dialog.close();
+                synchronized (dialog) {
+                    dialog.notify();
+                }
                 return value;
             });
+
+            UI ui = UI.getCurrent(); // in the UI thread, get the UI instance
+            new Thread(() -> { // create a worker thread to run the continuation as we will have to release the UI thread
+                UI.setCurrent(ui); // in case the command uses 'UI.getCurrent()' assign the parent thread's UI to this new child thread.
+                synchronized (dialog) {
+                    try {
+                        Boolean answer = future.get(); // wait for the user to complete the 'Boolean' future (that is, make a decision)
+                        ui.access(() -> {
+                            consumer.accept(answer);
+                            ui.push();
+                        });
+                    } catch (InterruptedException | ExecutionException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start(); // one may consider instead using a thread pool, see ExecutorService for example.
+
+            dialog.open();
         }
     }
-
 
     public BlockingDialog(){
 
