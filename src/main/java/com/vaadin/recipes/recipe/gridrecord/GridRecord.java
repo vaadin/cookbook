dcabd75533c1b7b7a44e62793a31dc407e83b5e0
Sebastian Kuehnau <seb@vaadin.com>
2021-12-06 09:32:54 +0100

    add some more use cases with LitRenderer

--- src/main/java/com/vaadin/recipes/recipe/gridrecord/GridRecord.java
+++ src/main/java/com/vaadin/recipes/recipe/gridrecord/GridRecord.java
@@ -1,12 +1,14 @@
 package com.vaadin.recipes.recipe.gridrecord;
 
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.data.renderer.LitRenderer;
 import com.vaadin.flow.router.Route;
 import com.vaadin.recipes.recipe.Metadata;
 import com.vaadin.recipes.recipe.Recipe;
 import com.vaadin.recipes.recipe.Tag;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.List;
 
 @Route("grid-record")
@@ -18,9 +20,9 @@ import java.util.List;
 public class GridRecord extends Recipe {
 
     private final List<Person> personList = List.of(
-            new Person("Donald Duck", LocalDateTime.of(1952, 06, 25, 0, 0)),
-            new Person("Micky Mouse", LocalDateTime.of(1654, 12, 5, 0, 0)),
-            new Person("Goofy", LocalDateTime.of(2020, 1, 14, 0, 0))
+            new Person("Donald Duck", LocalDate.of(1952, 06, 25)),
+            new Person("Micky Mouse", LocalDate.of(1654, 12, 5)),
+            new Person("Goofy", LocalDate.of(2020, 1, 14))
     );
 
     public GridRecord() {
@@ -35,10 +37,15 @@ public class GridRecord extends Recipe {
         Grid<Person> personGrid = new Grid<>();
         personGrid.addColumn(Person::name).setHeader("Name");
         personGrid.addColumn(person -> person.birthday).setHeader("Birthday");
+        personGrid.addColumn(
+                LitRenderer
+                        .<Person>of("<span>${item.value}</span>")
+                        .withProperty("value", person -> Period.between(person.birthday, LocalDate.now()).getYears()))
+                .setHeader("Age");
 
         personGrid.setItems(personList);
         add(personGrid);
     }
 
-    public record Person(String name, LocalDateTime birthday) {}
+    public record Person(String name, LocalDate birthday) {}
 }
