f7cb438b6fa3eba303464c490f11f8a673354c83
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2022-04-19 11:46:58 +0300

    Remove client-side Tag[]

--- src/main/java/com/vaadin/recipes/recipe/Tag.java
+++ src/main/java/com/vaadin/recipes/recipe/Tag.java
@@ -1,8 +1,6 @@
 package com.vaadin.recipes.recipe;
 
 public enum Tag {
-    // NOTE: These need to be copied to recipe-list-view.ts until
-    // https://github.com/vaadin/flow/issues/8963 is fixed
     JAVA,
     TYPE_SCRIPT,
     HILLA,
