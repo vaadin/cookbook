2543e2656655b94085045bff07a2055bc3b48bc7
Artur Signell <artur@vaadin.com>
2022-06-15 14:30:24 +0300

    Allow linking to search results
    Fixes #143

--- frontend/util.ts
+++ frontend/util.ts
@@ -1,3 +1,15 @@
 export const getSimpleName = (fullName: string) => {
   return fullName.substring(fullName.lastIndexOf("/") + 1);
 };
+
+export const replaceQueryParameter = (key:string,value:string) => {
+  const params = new URLSearchParams(window.location.search);
+  if (value === "") {
+    params.delete(key);
+  }else {
+    params.set(key,value);
+  }
+
+  const newLocation =  `${window.location.origin}${window.location.pathname}?${params.toString()}`;
+  history.replaceState('','', newLocation);
+}
