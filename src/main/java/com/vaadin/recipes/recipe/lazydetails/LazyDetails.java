1a7e7b518903fff8925d417ffe022ed75ac48148
Leif Åstrand <leif@vaadin.com>
2022-08-19 16:12:42 +0300

    Show how to lazily initialize Details content (#267)

---
+++ src/main/java/com/vaadin/recipes/recipe/lazydetails/LazyDetails.java
@@ -0,0 +1,32 @@
+package com.vaadin.recipes.recipe.lazydetails;
+
+import com.vaadin.flow.component.details.Details;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.router.Route;
+import com.vaadin.recipes.recipe.Metadata;
+import com.vaadin.recipes.recipe.Recipe;
+import com.vaadin.recipes.recipe.Tag;
+
+@Route("lazy-details")
+@Metadata(
+    howdoI = "Initialize Details content only when opened",
+    description = "Avoid initializing the content of a hidden Details panel until it is opened.",
+    tags = {Tag.FLOW, Tag.PERFORMANCE}
+)
+public class LazyDetails extends Recipe {
+    public LazyDetails() {
+        Span placeholder = new Span("Please wait...");
+
+        Details details = new Details("Expand me", placeholder);
+        details.addOpenedChangeListener(event -> {
+            // placeholder is attached until setContent below has been run
+            if (event.isOpened() && placeholder.isAttached()) {
+                Span realContent = new Span("This is the real content");
+
+                details.setContent(realContent);
+            }
+        });
+
+        add(details);
+    }
+}
