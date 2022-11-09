82ba7b3b21d4bf030d8040b3662e7d89bc5a863a
Zhe Sun <zhe@vaadin.com>
2022-11-09 15:51:47 +0200

    chore: update to Vaadin 24

--- src/main/java/com/vaadin/recipes/data/RecipeInfo.java
+++ src/main/java/com/vaadin/recipes/data/RecipeInfo.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Set;
 import javax.annotation.Nullable;
-import javax.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 
 public class RecipeInfo {
     @JsonIgnore
