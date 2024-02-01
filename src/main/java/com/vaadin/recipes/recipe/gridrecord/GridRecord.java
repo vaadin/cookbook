233ae42b94eb884e7ef1a24f2e912eb8c05c9908
Matti Tahvonen <matti@vaadin.com>
2024-02-01 09:07:31 +0200

    Records in grid update (#321)
    Alternative way

--- src/main/java/com/vaadin/recipes/recipe/gridrecord/GridRecord.java
+++ src/main/java/com/vaadin/recipes/recipe/gridrecord/GridRecord.java
@@ -6,6 +6,8 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.recipes.recipe.Metadata;
 import com.vaadin.recipes.recipe.Recipe;
 import com.vaadin.recipes.recipe.Tag;
+import org.vaadin.firitin.components.grid.VGrid;
+
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.List;
@@ -14,9 +16,12 @@ import java.util.List;
 @Metadata(
     howdoI = "Display a Java record in a Vaadin Grid",
     description = "Record types need separate handling when adding columns to the Vaadin Grid",
-    tags = { Tag.GRID, Tag.JAVA }
+    tags = { Tag.GRID, Tag.JAVA },
+    addons = "Viritin;https://vaadin.com/directory/component/flow-viritin"
 )
 public class GridRecord extends Recipe {
+
+    public record Person(String name, LocalDate birthday) {}
 
     private final List<Person> personList = List.of(
         new Person("Donald Duck", LocalDate.of(1952, 06, 25)),
@@ -25,29 +30,29 @@ public class GridRecord extends Recipe {
     );
 
     public GridRecord() {
-        // would not work alone
-        // Grid<Person> personGrid = new Grid<>(Person.class);
-
-        // would throw an exception java.lang.IllegalArgumentException: Can't resolve
-        // property name 'name' from
-        // 'Property set for bean com.vaadin.recipes.recipe.gridrecord.GridRecord$Person'
-        // personGrid.addColumn("name");
-
-        // here are possible options
-        Grid<Person> personGrid = new Grid<>();
-        personGrid.addColumn(Person::name).setHeader("Name");
-        personGrid.addColumn(person -> person.birthday).setHeader("Birthday");
-        personGrid
-            .addColumn(
-                LitRenderer
-                    .<Person>of("<span>${item.value}</span>")
-                    .withProperty("value", person -> Period.between(person.birthday, LocalDate.now()).getYears())
-            )
-            .setHeader("Age");
-
+        // Grid in core does not support defining columns from the
+        // type like with Java Beans, with VGrid from Viritin add-on
+        // it works as expected
+        Grid<Person> personGrid = new VGrid<>(Person.class);
         personGrid.setItems(personList);
+
+        // With core Grid, you need to manually define columns
+        boolean noViritin = false;
+        if(noViritin) {
+            personGrid = new Grid<>();
+            personGrid.addColumn(Person::name).setHeader("Name");
+            personGrid.addColumn(person -> person.birthday).setHeader("Birthday");
+            personGrid
+                .addColumn(
+                    LitRenderer
+                        .<Person>of("<span>${item.value}</span>")
+                        .withProperty("value", person -> Period.between(person.birthday, LocalDate.now()).getYears())
+                )
+                .setHeader("Age");
+            personGrid.setItems(personList);
+        }
+
         add(personGrid);
     }
 
-    public record Person(String name, LocalDate birthday) {}
 }
