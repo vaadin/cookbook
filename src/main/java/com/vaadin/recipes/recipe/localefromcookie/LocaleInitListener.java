d9a002f7b25048cedbd70d2d60c16c51f3e60f42
Leif Åstrand <leif@vaadin.com>
2024-09-05 10:28:48 +0300

    Set locale based on a cookie

---
+++ src/main/java/com/vaadin/recipes/recipe/localefromcookie/LocaleInitListener.java
@@ -0,0 +1,36 @@
+package com.vaadin.recipes.recipe.localefromcookie;
+
+import java.util.Locale;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import org.springframework.stereotype.Component;
+
+import com.vaadin.flow.server.ServiceInitEvent;
+import com.vaadin.flow.server.VaadinRequest;
+import com.vaadin.flow.server.VaadinServiceInitListener;
+
+import jakarta.servlet.http.Cookie;
+
+/*
+ * In a regular application, this could be simpler as as @Bean method in Application.java 
+ */
+@Component
+public class LocaleInitListener implements VaadinServiceInitListener {
+    static final String COOKIE_NAME = "my-app-locale";
+
+    @Override
+    public void serviceInit(ServiceInitEvent serviceInit) {
+        serviceInit.getSource().addSessionInitListener(sessionInit -> {
+            findCookie(sessionInit.getRequest()).ifPresent(cookie -> {
+                Locale locale = new Locale(cookie.getValue());
+                sessionInit.getSession().setLocale(locale);
+            });
+        });
+    }
+
+    static Optional<Cookie> findCookie(VaadinRequest request) {
+        return Stream.of(request.getCookies()).filter(cookie -> cookie.getName()
+                .equals(LocaleInitListener.COOKIE_NAME)).findAny();
+    }
+}
