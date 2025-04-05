0cb76bb26c48e7384f46f228a58befdf966eac54
Leif Åstrand <leif@vaadin.com>
2025-04-05 23:38:09 +0300

    Mark Audio.java as a resource (#344)
    Co-authored-by: Manuel Carrasco Moñino <manolo@vaadin.com>

--- src/main/java/com/vaadin/recipes/recipe/embedaudio/EmbedAudio.java
+++ src/main/java/com/vaadin/recipes/recipe/embedaudio/EmbedAudio.java
@@ -5,7 +5,8 @@ import com.vaadin.recipes.recipe.Metadata;
 import com.vaadin.recipes.recipe.Recipe;
 
 @Route("embed-audio")
-@Metadata(howdoI = "Embed an audio player", description = "Shows how you can embed an <audio> tag to allow the user to play audio")
+@Metadata(howdoI = "Embed an audio player", description = "Shows how you can embed an <audio> tag to allow the user to play audio", sourceFiles = {
+        "Audio.java" })
 public class EmbedAudio extends Recipe {
 
     public EmbedAudio() {
