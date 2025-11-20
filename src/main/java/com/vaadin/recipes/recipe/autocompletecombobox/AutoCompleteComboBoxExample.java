a06f248a12b2757bb5de5bf3f66e7c3f75c8870a
Herberts Markuns <herberts@vaadin.com>
2025-11-20 12:00:39 +0200

    Add a recipe for auto-complete combo box

---
+++ src/main/java/com/vaadin/recipes/recipe/autocompletecombobox/AutoCompleteComboBoxExample.java
@@ -0,0 +1,29 @@
+package com.vaadin.recipes.recipe.autocompletecombobox;
+
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.Route;
+import com.vaadin.recipes.recipe.Metadata;
+import com.vaadin.recipes.recipe.Recipe;
+
+@Route("autocomplete-combo-box")
+@Metadata(
+  howdoI = "Create an auto-complete ComboBox",
+  description = "Learn how to create a combo box for which value is auto-selected if the filter text matches exactly one value.",
+  sourceFiles = {"AutoCompleteComboBox.java"}
+)
+public class AutoCompleteComboBoxExample extends Recipe {
+
+    public AutoCompleteComboBoxExample() {
+        add(new Div("Enter text and press Tab, once filter only matches a single item."));
+
+        var comboBox = new AutoCompleteComboBox<ItemWithText>();
+        comboBox.setLabel("Auto-complete ComboBox");
+        comboBox.setItems(new ItemWithText(1, "Item Root 1"), new ItemWithText(2, "Item Root 2"), new ItemWithText(3, "Item Branch 3"));
+        comboBox.setItemLabelGenerator(ItemWithText::text);
+        add(comboBox);
+    }
+
+    public record ItemWithText(int id, String text) {
+
+    }
+}
