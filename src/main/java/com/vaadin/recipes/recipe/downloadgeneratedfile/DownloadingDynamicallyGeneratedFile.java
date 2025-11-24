4518d9d324050ee27fdf98215ed1e5f9ada7beca
Herberts Markuns <herberts@vaadin.com>
2025-11-24 16:25:46 +0200

    Rewrite "Offer a dynamic / generated file for download" to use DownloadHandler

--- src/main/java/com/vaadin/recipes/recipe/downloadgeneratedfile/DownloadingDynamicallyGeneratedFile.java
+++ src/main/java/com/vaadin/recipes/recipe/downloadgeneratedfile/DownloadingDynamicallyGeneratedFile.java
@@ -1,21 +1,25 @@
 package com.vaadin.recipes.recipe.downloadgeneratedfile;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.textfield.IntegerField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.streams.DownloadHandler;
+import com.vaadin.flow.server.streams.DownloadResponse;
 import com.vaadin.recipes.recipe.Metadata;
 import com.vaadin.recipes.recipe.Recipe;
 import com.vaadin.recipes.recipe.Tag;
-import org.vaadin.firitin.components.DynamicFileDownloader;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 
 @Route("dynamic-download")
 @Metadata(
         howdoI = "Offer a dynamic / generated file for download",
         description = "Shows how to allow the user to download a file generated on the server.",
-        tags = { Tag.DOWNLOAD }, addons = "Viritin;https://vaadin.com/directory/component/flow-viritin"
+        tags = { Tag.DOWNLOAD }
 )
 public class DownloadingDynamicallyGeneratedFile extends Recipe {
 
@@ -31,17 +35,24 @@ public class DownloadingDynamicallyGeneratedFile extends Recipe {
         var person = new PersonDto();
         binder.setBean(person);
 
-        // The actual download component and file generation
-        add(new DynamicFileDownloader("Download as JSON", outputStream -> {
+        var downloadHandler = DownloadHandler.fromInputStream(downloadEvent -> {
+            var outputValue = binder.getBean();
+            var outputStream = new ByteArrayOutputStream();
             // Using Jackson here to generate JSON to the output stream,
             // but could be just as well be XML, image or PDF
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.writeValue(outputStream, person);
-        }).withFileNameGenerator(r -> {
-            // Using a FileNameGenerator, you can customize the
-            // name of the downloaded file
-            return person.getName().replaceAll(" ", "_") + ".json";
-        }));
+            var objectMapper = new ObjectMapper();
+            objectMapper.writeValue(outputStream, outputValue);
+
+            // Convert output stream to input stream
+            var inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+            return new DownloadResponse(inputStream,
+                    name.getValue().replaceAll(" ", "_") + ".json", // dynamic file name
+                    "application/json", // content type
+                    -1); // content length (-1 = unknown)
+        });
+
+        var link = new Anchor(downloadHandler, "Download as JSON");
+        add(link);
     }
 
     public static class PersonDto {
