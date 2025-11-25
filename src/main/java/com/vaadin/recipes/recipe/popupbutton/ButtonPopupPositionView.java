00a229757b172f44148cf79ce156ba87a1765d26
Herberts Markuns <herberts@vaadin.com>
2025-11-25 15:46:50 +0200

    Rewrite "Show a popup next to a button when it is clicked" to also modern approach (Popover)

--- src/main/java/com/vaadin/recipes/recipe/popupbutton/ButtonPopupPositionView.java
+++ src/main/java/com/vaadin/recipes/recipe/popupbutton/ButtonPopupPositionView.java
@@ -4,6 +4,9 @@ import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.popover.Popover;
+import com.vaadin.flow.component.popover.PopoverPosition;
+import com.vaadin.flow.component.popover.PopoverVariant;
 import com.vaadin.flow.router.Route;
 import com.vaadin.recipes.recipe.Metadata;
 import com.vaadin.recipes.recipe.Recipe;
@@ -11,17 +14,32 @@ import com.vaadin.recipes.recipe.Recipe;
 @Route("button-popup")
 @Metadata(
     howdoI = "Show a popup next to a button when it is clicked",
-    description = "Code snippet for showing a dialog next to a button for context-sensitive help or actions. Uses the Vaadin Java API. "
+    description = "Code snippet for showing a popover / dialog next to a button for context-sensitive help or actions. Uses the Vaadin Java API. "
 )
 public class ButtonPopupPositionView extends Recipe {
-    private Button button = new Button("open dialog", this::openDialog);
-    private Dialog dialog = new Dialog(new Span("dialog opened"));
 
     public ButtonPopupPositionView() {
-        add(button);
+        add(createModernApproach());
+        add(createOldApproach());
     }
 
-    private void openDialog(ClickEvent<Button> buttonClickEvent) {
+    private Button createModernApproach() {
+        var button = new Button("Button + Popover (Modern / V24.5+)");
+
+        var popover = new Popover(new Span("Popover opened"));
+        popover.setTarget(button);
+        popover.addThemeVariants(PopoverVariant.ARROW);
+        popover.setPosition(PopoverPosition.END);
+
+        return button;
+    }
+
+    private Button createOldApproach() {
+        var dialog = new Dialog(new Span("Dialog opened"));
+        return new Button("Button + Dialog (pre V24.5)", event -> openOldApproachDialog(event, dialog));
+    }
+
+    private void openOldApproachDialog(ClickEvent<Button> buttonClickEvent, Dialog dialog) {
         dialog.open();
         /* position the dialog next to the button on the left */
         dialog
@@ -32,7 +50,7 @@ public class ButtonPopupPositionView extends Recipe {
                 "$0.$.overlay.$.overlay.style['top']= ($1.getBoundingClientRect().top - $1.getBoundingClientRect().height )+ 'px';" +
                 "$0.$.overlay.$.overlay.style['left']= ($1.getBoundingClientRect().left + $1.getBoundingClientRect().width) + 'px'",
                 dialog,
-                button
+                buttonClickEvent.getSource() // button
             );
         dialog.open();
     }
